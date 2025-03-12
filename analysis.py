ca0f12803bcb9904eb8b5f3d2bc7acba609a8345
Shishir Iyer <shiyer@ucsd.edu>
2025-03-11 17:04:56 -0700

    plot size timeseries

--- analysis.py
+++ analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 df = pd.read_excel("Fiji combined 2012-2023.xlsx")
 df = df[df["Boat owner"] != "N/A"]
@@ -7,4 +9,20 @@ df = df[df["Trip cost ($FJD)"] != "N/A"]
 df["Year"] = df["Date"].apply(lambda x : x.year)
 df["Month"] = df["Date"].apply(lambda x : x.month)
 
+df["year_month"] = df["Date"].dt.to_period('M')
+monthly_avg = df.groupby("year_month")["Fish size"].mean()
+
+monthly_avg.index = monthly_avg.index.to_timestamp()
+
 print(df)
+if not Path("fiji_combined.csv").is_file():
+    print("Outputting csv...")
+    df.to_csv("fiji_combined.csv")
+
+plt.figure(figsize=(10, 5))
+plt.plot(monthly_avg.index, monthly_avg.values, marker='o', linestyle='-')
+plt.xlabel('Date')
+plt.ylabel('Average Value')
+plt.title('Monthly Averages Over Time')
+plt.grid()
+plt.show()
